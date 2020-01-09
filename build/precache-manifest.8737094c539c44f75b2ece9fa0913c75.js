3a413ab036503112f7ad64f9f85819fd70f8b45d
zhanglei <1341992589@qq.com>
2020-01-09 20:11:36 +0800

    网站顶部图标和标题

---
+++ build/precache-manifest.8737094c539c44f75b2ece9fa0913c75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d7ae8c25b942afb68cc65b414dd298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9626d596e100afb9c039",
+    "url": "/static/css/2.44cca978.chunk.css"
+  },
+  {
+    "revision": "f376771dbcb902c22ec7",
+    "url": "/static/css/main.2f2fde6a.chunk.css"
+  },
+  {
+    "revision": "9626d596e100afb9c039",
+    "url": "/static/js/2.9a5280f6.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.9a5280f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f376771dbcb902c22ec7",
+    "url": "/static/js/main.a0d962b5.chunk.js"
+  },
+  {
+    "revision": "861e4233396253aee4bd",
+    "url": "/static/js/runtime-main.07fa7236.js"
+  },
+  {
+    "revision": "52cd0bb38f2e84879c7e17c93559aaac",
+    "url": "/static/media/bofang.52cd0bb3.png"
+  },
+  {
+    "revision": "f277f8de22f018cfd9c47460c1037d1d",
+    "url": "/static/media/zhugeyunmusic.f277f8de.png"
+  }
+]);
